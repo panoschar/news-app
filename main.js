704d710e1cabe1e5d3eccda3cf8eac147075d325
panos-charalampopoulos <panos10cha@gmail.com>
2022-05-19 03:50:59 +0300

    wallpapper attempt

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-fetch(`https://newsapi.org/v2/top-headlines?category=sports&apiKey=75aee15d7fea48038aee8d5dd15f8657`)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data.articles);
-    });
+
+
+function getNews() {
+ 
+    fetch(`https://newsapi.org/v2/everything?q=football&apiKey=75aee15d7fea48038aee8d5dd15f8657`)
+  
+.then((response) => response.json())
+.then(data => console.log(data.articles));
+        document.querySelector('main-news').innerHTML = data.articles;
+    
+    };
+ 
+
