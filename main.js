19f3e6612485a24360131db7767bc8d3c3f26c30
panos-charalampopoulos <panos10cha@gmail.com>
2022-05-19 21:03:45 +0300

    articles displayed

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 
 
-function getNews() {
- 
-    fetch(`https://newsapi.org/v2/everything?q=football&apiKey=75aee15d7fea48038aee8d5dd15f8657`)
-  
-.then((response) => response.json())
-.then(data => console.log(data.articles));
-        document.querySelector('main-news').innerHTML = data.articles;
-    
-    };
- 
 
+
+function displayNews() {
+    document.getElementById('main-news').innerHTML = "";
+
+    fetch(`https://newsapi.org/v2/everything?q=football&apiKey=75aee15d7fea48038aee8d5dd15f8657`)
+
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            for (let i = 0; i < data.articles.length; i++) {
+                document.getElementById('main-news').innerHTML  += `<li>${data.articles[i].content}</li>`
+
+            }
+        });
+
+};
+displayNews();
