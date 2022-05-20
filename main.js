e6d495818fb661c93d31f01b7d15dcc09eacce21
panos-charalampopoulos <panos10cha@gmail.com>
2022-05-21 02:29:47 +0300

    footer attempt

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-
-
-
-
 function displayNews() {
     document.getElementById('main-news').innerHTML = "";
 
@@ -12,6 +8,23 @@ function displayNews() {
             console.log(data)
             for (let i = 0; i < data.articles.length; i++) {
                 document.getElementById('main-news').innerHTML  += `<li>${data.articles[i].content}</li>`
+
+            }
+        });
+
+};
+displayNews();
+
+function displaySide() {
+    document.getElementById('side-news').innerHTML = "";
+
+    fetch(`https://newsapi.org/v2/everything?q=football&apiKey=75aee15d7fea48038aee8d5dd15f8657`)
+
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            for (let i = 0; i < data.articles.length; i++) {
+                document.getElementById('side-news').innerHTML  += `<li>${data.articles[i].content}</li>`
 
             }
         });
