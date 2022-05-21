70da790c982a9d96720d89a68c48cf59d4ff30b5
panos-charalampopoulos <panos10cha@gmail.com>
2022-05-21 18:54:01 +0300

    final commit

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-function displayNews() {
+
+ function displayNews() {
+    document.getElementById('main-news').innerHTML = "";
+
+    fetch(`https://newsapi.org/v2/everything?q=football&apiKey=75aee15d7fea48038aee8d5dd15f8657`)
+
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            for (let i = 0; i < data.articles.length; i++) {
+                document.getElementById('main-news').innerHTML  += `<h4>${data.articles[i].title}</h4>`
+                document.getElementById('main-news').innerHTML  += `<p>${data.articles[i].description}</p>`
+                document.getElementById('main-news').innerHTML  += `<p>${data.articles[i].content}</p>`
+            }
+        });
+
+};
+displayNews();
+
+
+
+
+
+
+
+
+
+function displaySide() {
     document.getElementById('main-news').innerHTML = "";
 
     fetch(`https://newsapi.org/v2/everything?q=football&apiKey=75aee15d7fea48038aee8d5dd15f8657`)
@@ -8,23 +35,6 @@ function displayNews() {
             console.log(data)
             for (let i = 0; i < data.articles.length; i++) {
                 document.getElementById('main-news').innerHTML  += `<li>${data.articles[i].content}</li>`
-
-            }
-        });
-
-};
-displayNews();
-
-function displaySide() {
-    document.getElementById('side-news').innerHTML = "";
-
-    fetch(`https://newsapi.org/v2/everything?q=football&apiKey=75aee15d7fea48038aee8d5dd15f8657`)
-
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            for (let i = 0; i < data.articles.length; i++) {
-                document.getElementById('side-news').innerHTML  += `<li>${data.articles[i].content}</li>`
 
             }
         });
